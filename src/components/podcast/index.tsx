7e9673ebb22be6788da32e1cb0c00df249ccc216
Marcus Kullman <marcus@marcuskullman.com>
2025-08-03 09:50:32 +0200

    sa

--- src/components/podcast/index.tsx
+++ src/components/podcast/index.tsx
@@ -4,37 +4,39 @@ import styles from "./podcast.module.scss"
 
 export const Podcast: FC = () => {
   return (
-    <div className={styles.podcast}>
-      <figure className={styles.figure}>
-        <img
-          src={Banner}
-          className={styles.image}
-          alt="Marcus Kullman Podcast"
-        />
-        <div className={styles.links}>
-          <p className={styles.description}>marcus@marcuskullman.com</p>
-          <a
-            href="https://open.spotify.com/show/2JjIvkufqT8WGaggtlHOy9?si=30d85af9c45f483f"
-            className={styles.link}
-          >
-            Podcast
-          </a>
-          {" | "}
-          <a
-            href="https://chatgpt.com/g/g-68721b3f753081918f555423389902d2-marcus-kullman"
-            className={styles.link}
-          >
-            ChatGPT-avatar
-          </a>
-          {" | "}
-          <a href="https://www.bokaro.se" className={styles.link}>
-            Min bokhandel
-          </a>
-        </div>
-      </figure>
+    <>
+      <div className={styles.podcast}>
+        <figure className={styles.figure}>
+          <img
+            src={Banner}
+            className={styles.image}
+            alt="Marcus Kullman Podcast"
+          />
+          <div className={styles.links}>
+            <p className={styles.description}>marcus@marcuskullman.com</p>
+            <a
+              href="https://open.spotify.com/show/2JjIvkufqT8WGaggtlHOy9?si=30d85af9c45f483f"
+              className={styles.link}
+            >
+              Podcast
+            </a>
+            {" | "}
+            <a
+              href="https://chatgpt.com/g/g-68721b3f753081918f555423389902d2-marcus-kullman"
+              className={styles.link}
+            >
+              ChatGPT-avatar
+            </a>
+            {" | "}
+            <a href="https://www.bokaro.se" className={styles.link}>
+              Bokaro
+            </a>
+          </div>
+        </figure>
+      </div>
       <div className={styles.vignette}>
         <img src={Banner} className={styles.image2} alt="" />
       </div>
-    </div>
+    </>
   )
 }
